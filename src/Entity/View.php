00870f1beea2aaa10d2352260045a6b4ea7254d4
iamdavi <domato99.dom@gmail.com>
2023-09-23 23:26:34 +0200

    Creadas entidades de 'Like' y 'View'

---
+++ src/Entity/View.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ViewRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ViewRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class View extends EntityBase
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'views')]
+    private ?User $user = null;
+
+    #[ORM\ManyToOne(inversedBy: 'views')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Post $Post = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getPost(): ?Post
+    {
+        return $this->Post;
+    }
+
+    public function setPost(?Post $Post): static
+    {
+        $this->Post = $Post;
+
+        return $this;
+    }
+}
